1c0ad857e2240950dd970cb2f9641a824ab92bfc
jrcowan3 <jimmy@stitchedin.com>
2023-06-03 00:21:40 -0500

    6/3

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -55,7 +55,7 @@ export default function NavBar({
         >
           Today
         </Link>
-        {tomorrow !== '6/3' ? (
+        {tomorrow !== '6/4' ? (
           <Link
             href={`/prayer/${tomorrow}/${session}`}
             className="grow border text-center align-middle leading-10"
