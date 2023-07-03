84120b371ad54fc4778a326629e38285b4ba7600
jrcowan3 <jimmy@stitchedin.com>
2023-07-03 09:57:02 -0500

    add rest of days

--- src/data/july.tsx
+++ src/data/july.tsx
@@ -1,6 +1,442 @@
 // july 9th is communion sunday
 const july: Month = {
   month: 7,
-  days: [],
+  days: [
+    {
+      day: 1,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [89],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [90],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 2,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [91],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [92],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 3,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [93],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [94],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 4,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [95, 96],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [97],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 5,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [98, 99],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [100],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 6,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [101],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [102],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 7,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [103],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [104],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 8,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 9,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [105],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [106],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 10,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [107],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [108],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 11,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [109, 110],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [111],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 12,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [112, 113],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [114],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 13,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [115],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [116],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 14,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [117],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [118],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 15,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: ['119:1-24'],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: ['119:25-48'],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 16,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: ['119:49-72'],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: ['119:73-88'],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 17,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: ['119:89-112'],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: ['119:113-136'],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 18,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: ['119:137-152'],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: ['119:153-176'],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 19,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [120, 121],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [122, 123],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 20,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [124, 125],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [126],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 21,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [127, 128],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [129],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 22,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [130, 131],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [132, 133],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 23,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [134, 135],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [136],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 24,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [137, 138],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [139],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 25,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [140, 141],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [142, 143],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 26,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [144],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [145],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 27,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [146],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [147],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 28,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [148, 149],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [150],
+          collects: [],
+        },
+      ],
+    },
+    {
+      day: 29,
+      sessions: [
+        {
+          session: 'morning',
+          psalms: [],
+          collects: [],
+        },
+        {
+          session: 'evening',
+          psalms: [],
+          collects: [],
+        },
+      ],
+    },
+  ],
 };
 export default july;
